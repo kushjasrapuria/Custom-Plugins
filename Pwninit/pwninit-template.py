578b2755d1eeb2477d0b5cb726ba46d570330467
/x90Slide <kushjasrapuria03@gmail.com>
2024-10-27 20:36:22 +0530

    Create pwninit-template.py

---
+++ Pwninit/pwninit-template.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from pwn import *
+
+{bindings}
+
+gs = ('''
+init-pwndbg
+continue
+''')
+
+context.binary = {bin_name}
+
+def conn():
+    if args.GDB:
+        return gdb.debug({proc_args}, gdbscript=gs)
+    elif args.LOCAL:
+        return process({proc_args})
+    else:
+        return remote("addr", 1337)
+
+r = conn()
+
+def main():
+
+    # good luck pwning :)
+
+    r.interactive()
+
+
+if __name__ == "__main__":
+    main()
